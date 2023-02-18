0150f722361af1c3fe07fe6aa6ce2b4fbe0727f0
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-02-18 06:52:28 +0200

    Update views.py

--- jeudi16/accounts/views.py
+++ jeudi16/accounts/views.py
@@ -1,18 +1,42 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.core.validators import validate_email
+from django.db.models import Q
 
 # Create your views here.
 def f_signup(request):
-	"""
-	username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-	"""
+	error = False
+	message = ""
+
 	if request.method == "POST":
 		username = request.POST.get('username', None)
-		useremail = request.POST.get('useremail', None)
-		print(username)
-	return render(request,"accounts/v_signup.html")
+		email = request.POST.get('useremail', None)
+		password = request.POST.get('password', None)
+
+		try:
+		 	validate_email(email)
+		except:
+			error = True
+			message = "Entrer un email valide !!!"
+		
+		user = User.objects.filter(Q(email = email)| Q(username = username)).first()
+
+		if user:
+			error = True
+			message = "email ou pseudo existant !!!!"
+		#print(username)
+		if error == False:
+			print("success")
+			user = User(
+				username = username,
+				email = email,
+				)
+			user.save()
+	context = {
+		'error': error,
+		'message': message
+	}
+	return render(request,"accounts/v_signup.html", context)
 
 def f_account(request):
 	return render(request,"accounts/v_account.html")
