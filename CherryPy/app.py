d0550adf6f33399e32200fddb6466b9632fe52c3
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-02-22 14:16:52 +0200

    Update app.py

--- CherryPy/app.py
+++ CherryPy/app.py
@@ -1,8 +1,37 @@
 import cherrypy
 
-class HelloWorld(object):
+class TodoList(object):
+    def __init__(self):
+        self.tasks = []
+    
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        # Afficher la liste des tâches
+        html = "<h1>Todolist</h1>"
+        html += "<ul>"
+        for i, task in enumerate(self.tasks):
+            html += f"<li>{task} <a href='/remove_task?task_id={i}'>Supprimer</a></li>"
+        html += "</ul>"
+        html += "<form method='post' action='/add_task'>"
+        html += "<input type='text' name='task' />"
+        html += "<input type='submit' value='Ajouter' />"
+        html += "</form>"
+        return html
+    
+    @cherrypy.expose
+    def add_task(self, task):
+        # Ajouter une nouvelle tâche
+        self.tasks.append(task)
+        raise cherrypy.HTTPRedirect('/')
+    
+    @cherrypy.expose
+    def remove_task(self, task_id):
+        # Supprimer une tâche
+        task_id = int(task_id)
+        if task_id >= 0 and task_id < len(self.tasks):
+            del self.tasks[task_id]
+        raise cherrypy.HTTPRedirect('/')
+    
+if __name__ == '__main__':
+    cherrypy.quickstart(TodoList())
 
-cherrypy.quickstart(HelloWorld())
