ae5011f772bf72302dab649cf5fd5a3e3cca4e6c
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-02-23 06:07:37 +0200

    Update views.py

--- Django/accounts/views.py
+++ Django/accounts/views.py
@@ -1,45 +1,69 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.core.validators import validate_email
 from django.db.models import Q
 
 # Create your views here.
 def f_signup(request):
-	error = False
-	message = ""
-
+	error, message = False, ""
 	if request.method == "POST":
 		username = request.POST.get('username', None)
+		first_name = request.POST.get('firstname', None)
 		email = request.POST.get('useremail', None)
 		password = request.POST.get('password', None)
 
 		try:
 		 	validate_email(email)
 		except:
-			error = True
-			message = "Entrer un email valide !!!"
+			error, message = True, "Entrer un email valide !!!"
 		
-		user = User.objects.filter(Q(email = email)| Q(username = username)).first()
+		user = User.objects.filter(Q(email = email) | Q(username = username)).first()
 
 		if user:
 			error = True
 			message = "email ou pseudo existant !!!!"
-		#print(username)
+
 		if error == False:
 			print("success")
 			user = User(
 				username = username,
 				email = email,
+				first_name = first_name,
 				)
 			user.save()
 	context = {
 		'error': error,
-		'message': message
+		'message': message,
 	}
 	return render(request,"accounts/v_signup.html", context)
 
+@login_required(login_url='signin')
 def f_account(request):
 	return render(request,"accounts/v_account.html")
 
 def f_signin(request):
+	if request.method == "POST":
+		email = request.POST.get('useremail', None)
+		password = request.POST.get('password', None)
+
+		user = User.objects.filter(email = email).first()
+		
+		if user:
+			auth_user = authenticate(username = user.username, password = password)
+			if auth_user:
+				login(request,auth_user)
+
+				return redirect('account')
+			else:
+				print("mdp incorrect")
+		else:
+			print("user not exist")
+
 	return render(request,"accounts/v_signin.html")
+
+
+
+def logout(request):
+	logout(request)
